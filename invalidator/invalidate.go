7e392c3f90a3f51c1ac5e71da7a849cc952e44b9
Carl Johnson <me@carlmjohnson.net>
2018-02-15 14:42:43 -0500

    Invalidator: Wait for invalidation to finish

--- invalidator/invalidate.go
+++ invalidator/invalidate.go
@@ -15,10 +15,11 @@ import (
 )
 
 type Invalidator struct {
-	s     *session.Session
+	cf    *cloudfront.CloudFront
 	dist  string
 	ref   string
 	paths []string
+	wait  time.Duration
 }
 
 func FromArgs(args []string) (inv *Invalidator, err error) {
@@ -27,6 +28,7 @@ func FromArgs(args []string) (inv *Invalidator, err error) {
 	fl.StringVar(&inv.dist, "dist", "", "CloudFront distribution ID")
 	fl.StringVar(&inv.ref, "ref", "",
 		"CloudFront 'CallerReference', a unique identifier for this invalidation request. (default: Unix timestamp)")
+	fl.DurationVar(&inv.wait, "wait", 15*time.Minute, "Time out for waiting on invalidation to complete. Set to 0 to exit without waiting.")
 	fl.Usage = func() {
 		fmt.Fprintf(os.Stderr,
 			`Usage of boreas:
@@ -64,26 +66,37 @@ Options:
 		inv.ref = fmt.Sprintf("%d", time.Now().Unix())
 	}
 
-	inv.s, err = session.NewSession()
+	s, err := session.NewSession()
+	inv.cf = cloudfront.New(s)
 	return
 }
 
 func (inv *Invalidator) Execute() error {
-	svc := cloudfront.New(inv.s)
-	input := &cloudfront.CreateInvalidationInput{
-		DistributionId: &inv.dist,
-		InvalidationBatch: &cloudfront.InvalidationBatch{
-			CallerReference: &inv.ref,
-			Paths:           makepaths(inv.paths),
-		},
-	}
-
-	result, err := svc.CreateInvalidation(input)
+	id, err := inv.Invalidate()
 	if err != nil {
 		return err
 	}
-	log.Printf("Invalidation ID: %q", *result.Invalidation.Id)
-	return nil
+	log.Printf("Invalidation ID: %q", id)
+
+	if inv.wait < 1 {
+		return nil
+	}
+
+	fmt.Print("Invalidation in progress")
+	defer fmt.Println()
+	deadline := time.Now().Add(inv.wait)
+	for deadline.After(time.Now()) {
+		time.Sleep(10 * time.Second)
+		done, err := inv.Done(id)
+		if err != nil {
+			return err
+		}
+		if done {
+			return nil
+		}
+		fmt.Print(".")
+	}
+	return fmt.Errorf("wait timeout of %v exceeded", inv.wait)
 }
 
 func makepaths(paths []string) *cloudfront.Paths {
@@ -96,4 +109,30 @@ func makepaths(paths []string) *cloudfront.Paths {
 		Items:    items,
 		Quantity: &quantity,
 	}
+}
+
+func (inv *Invalidator) Invalidate() (id string, err error) {
+	result, err := inv.cf.CreateInvalidation(&cloudfront.CreateInvalidationInput{
+		DistributionId: &inv.dist,
+		InvalidationBatch: &cloudfront.InvalidationBatch{
+			CallerReference: &inv.ref,
+			Paths:           makepaths(inv.paths),
+		},
+	})
+	if err != nil {
+		return "", err
+	}
+
+	return *result.Invalidation.Id, nil
+}
+
+func (inv *Invalidator) Done(id string) (done bool, err error) {
+	result, err := inv.cf.GetInvalidation(&cloudfront.GetInvalidationInput{
+		DistributionId: &inv.dist,
+		Id:             &id,
+	})
+	if err != nil {
+		return false, err
+	}
+	return *result.Invalidation.Status == "Completed", nil
 }
